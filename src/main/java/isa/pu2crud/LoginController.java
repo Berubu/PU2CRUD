15718da17f6accf4b57e51902d913989fa138ae8
Berubu <93852000+Berubu@users.noreply.github.com>
2024-03-18 23:43:21 -0600

    LoginController.java

--- src/main/java/isa/pu2crud/LoginController.java
+++ src/main/java/isa/pu2crud/LoginController.java
@@ -1,2 +1,41 @@
-package isa.pu2crud;public class LoginController {
+package isa.pu2crud;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    private FXPrincipalController fxPrincipalController;
+
+    public void setFxPrincipalController(FXPrincipalController fxPrincipalController) {
+        this.fxPrincipalController = fxPrincipalController;
+    }
+
+    public void login() {
+        if (fxPrincipalController == null) {
+            System.out.println("Error: FXPrincipalController no está inicializado");
+            return;
+        }
+
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if (isValidCredentials(username, password)) {
+            System.out.println("Inicio de sesión exitoso");
+            fxPrincipalController.showStage(); // Llamar al método showStage en el cotrolador principal
+        } else {
+            System.out.println("Nombre de usuario o contraseña incorrectos");
+        }
+    }
+
+    private boolean isValidCredentials(String username, String password) {
+        // Validación de credenciales
+        return username.equals("isa") && password.equals("isa");
+    }
 }
